2e7d660b4524bc5bc3e07fcf01f9a3463d36da20
Maria <mtornabene05@gmail.com>
2022-04-11 22:07:56 -0400

    gallery testing complete

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,27 +1,17 @@
-/* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
 import { capitalizeFirstLetter } from '../../utils/helpers';
-import photo from "../../assets/small/commercial/0.jpg";
+import PhotoList from '../PhotoList';
 
 
 function Gallery(props) {
 
-    const currentCategory ={
-        name: "commercial",
-        description: "Photos of grocery stores, food trucks, and other commercial projects",
-    };
+    const { currentCategory } = props;
     
     return (
         <section>
-            <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
-            <p>{currentCategory.name}</p>
-            <div className='flex-row'>
-                <img
-                    src={photo}
-                    alt="Commercial Example"
-                    className="img-thumbnail mx-1"
-                />
-            </div>
+            <h1 data-testid="h1tag">{capitalizeFirstLetter(currentCategory.name)}</h1>
+            <p>{currentCategory.description}</p>
+            <PhotoList category={currentCategory.name} />
         </section>
     );
 }
