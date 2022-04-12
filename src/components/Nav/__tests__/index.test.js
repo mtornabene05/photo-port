886d1ab9fa59118d5b5211f07aaf539c9badafe5
Maria <mtornabene05@gmail.com>
2022-04-11 23:37:36 -0400

    contact testing complete

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -1,4 +1,5 @@
 /* eslint-disable testing-library/prefer-screen-queries */
+// __tests__/Nav.test.js with hard coded categories
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
@@ -6,33 +7,32 @@ import Nav from '..';
 
 afterEach(cleanup);
 
-describe('Nav component', () => {
-    //baseline test
-    it('renders', () => {
-        render(<Nav />);
-    });
-    //snapshot test
-    it('matches snapshot', () => {
-        const { asFragment } =render(<Nav />);
-        expect(asFragment()).toMatchSnapshot();
-    });
-});
+describe('Nav component renders', () => {
+  it('renders', () => {
+    render(<Nav />);
+  });
 
-describe('emoji is visible', () => {
-    it('inserts emoji into h2', () => {
-        //Arrange
-        const { getByLabelText } = render(<Nav />);
-        //assert
-        expect(getByLabelText('camera')).toHaveTextContent('📸');
-    })
+  it('matches snapshot', () => {
+    const { asFragment } = render(<Nav />);
+    
+    expect(asFragment()).toMatchSnapshot();
+  });
 })
 
+describe('emoji is visible', () => {
+  it('inserts emoji into the h2', () => {
+  const { getByLabelText } = render(<Nav />);
+
+  expect(getByLabelText('camera')).toHaveTextContent('📸');
+  });
+})  
+
 describe('links are visible', () => {
-    it('inserts text into the links', () => {
-        //arrange
-        const { getByTestId } =render(<Nav />);
-        //assert
-        expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
-        expect(getByTestId('about')).toHaveTextContent('About me');
-    });
+  it('inserts text into the links', () => {
+    const { getByTestId } = render(<Nav />);
+
+    expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+    expect(getByTestId('about')).toHaveTextContent('About me');
+  });
+
 })
